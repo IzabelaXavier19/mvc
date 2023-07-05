2ad00a05f011676709fc7553a5f85e624e05f87d
IzabelaXavier19 <izabelasilva07@hotmail.com>
2023-07-05 15:00:27 -0300

    fazendo conexão com mongoDB

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 
-mongoose.connect();
+mongoose.connect("mongodb+srv://admin:ertyhb45@cluster0.gdxzyaz.mongodb.net/senac2");
 //A função connect guarda a string de conexão dentro dos parênteses.
 
-let db = mongoose.connect;
+let db = mongoose.connection;
 // Agora criamos uma variavel que guarda a ação de conectar nosso usuario com o mongoDB.
-module.exports = db;
+
+export default db;
